1720df34424386c89ccf35aa057913aa75994f5b
DanielScariot <faroeste.telefone@gmail.com>
2015-06-09 16:28:38 -0300

    Struct Monster
    Adicionada a estrutura dos monstros

---
+++ Monsters.h
@@ -0,0 +1,8 @@
+struct Monsters
+{
+    int health;         //VIDA DO MONSTRO
+    int speed;          //VELOCIDADE DO MONSTRO
+    int xlocation;      //LOCALIZAÇÃO X DO MONSTRO
+    int ylocation;      //LOCALIZAÇÃO Y DO MONSTRO
+    bool stillalive;    //TRUE = VIVO    FALSE = MORTO
+};
