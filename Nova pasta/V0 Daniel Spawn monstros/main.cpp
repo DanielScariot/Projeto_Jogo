7b10065fb887a54090ffdc699562d761c7a878c8
DanielScariot <faroeste.telefone@gmail.com>
2015-06-11 21:56:02 -0300

    V1.0 It´s alive
    Apenas o começo da inicialização do inicio do ritual para o conceito da
    vida do universo e tudo mais..
    A.. e tem algumas bolinhas andando pelo mapa, isso é importante

--- Nova pasta/V0 Daniel Spawn monstros/main.cpp
+++ Nova pasta/V0 Daniel Spawn monstros/main.cpp
@@ -10,7 +10,7 @@ struct Monstro
     int xlocation;      //LOCALIZAÇÃO X DO MONSTRO
     int ylocation;      //LOCALIZAÇÃO Y DO MONSTRO
     bool stillalive;    //TRUE = VIVO    FALSE = MORTO
-}Monstro[10];
+} Monstro[10];
 
 const int LARGURA_TELA = 640;
 const int ALTURA_TELA = 480;
@@ -27,30 +27,45 @@ void createmonster(int n)
 
 void movemonster(int n)
 {
-     if ((Monstro[n].xlocation < 250) && (Monstro[n].ylocation < 225))
-     {
-          Monstro[n].ylocation++;
-     }
-     if ((Monstro[n].xlocation < 250) && (Monstro[n].ylocation == 225))
-     {
-          Monstro[n].xlocation++;
-     }
-     if ((Monstro[n].xlocation == 250) && (Monstro[n].ylocation > 125))
-     {
-          Monstro[n].ylocation--;
-     }
-     if ((Monstro[n].xlocation < 425) && (Monstro[n].ylocation == 125))
-     {
-          Monstro[n].xlocation++;
-     }
-     if ((Monstro[n].xlocation == 425) && (Monstro[n].ylocation < 325))
-     {
-          Monstro[n].ylocation++;
-     }
-     if ((Monstro[n].xlocation < 530) && (Monstro[n].ylocation == 325))
-     {
-          Monstro[n].xlocation++;
-     }
+    if ((Monstro[n].xlocation < 250) && (Monstro[n].ylocation < 225))
+    {
+        Monstro[n].ylocation++;
+        printf("\no monstro %d esta se mexendo\n", n);
+    }
+    if ((Monstro[n].xlocation < 250) && (Monstro[n].ylocation == 225))
+    {
+        Monstro[n].xlocation++;
+        printf("\no monstro %d esta se mexendo\n", n);
+    }
+    if ((Monstro[n].xlocation == 250) && (Monstro[n].ylocation > 125))
+    {
+        Monstro[n].ylocation--;
+        printf("\no monstro %d esta se mexendo\n", n);
+    }
+    if ((Monstro[n].xlocation < 425) && (Monstro[n].ylocation == 125))
+    {
+        Monstro[n].xlocation++;
+    }
+    if ((Monstro[n].xlocation == 425) && (Monstro[n].ylocation < 325))
+    {
+        Monstro[n].ylocation++;
+    }
+    if ((Monstro[n].xlocation < 530) && (Monstro[n].ylocation == 325))
+    {
+        Monstro[n].xlocation++;
+    }
+}
+void erasemonster(int n)
+{
+    al_draw_filled_circle(Monstro[n].xlocation, Monstro[n].ylocation, 18, al_map_rgb(255, 255, 0));
+}
+
+void drawmonster(int n)
+{
+    al_init_primitives_addon();
+    al_draw_filled_circle(Monstro[n].xlocation, Monstro[n].ylocation, 18, al_map_rgb(0, 0, 255));
+    printf("\nmonstro %d foi criado\n", n);
+    al_rest(0);
 }
 
 
@@ -61,7 +76,7 @@ int main(void)
     ALLEGRO_BITMAP *imagem = NULL;
     ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
 
-     if (!al_init())
+    if (!al_init())
     {
         fprintf(stderr, "Falha ao inicializar a Allegro.\n");
         return -1;
@@ -97,31 +112,7 @@ int main(void)
     }
 
     al_init_primitives_addon();
-    al_draw_line(640,480, 0, 0, al_map_rgb(0, 255, 0), 1000);
-
-
-
-    /*rectfill(screen,0,0,70,200,6);
-    rectfill(screen,0,200,230,250,6);
-    rectfill(screen,230,100,275,250,6);
-    rectfill(screen,275,100,400,150,6);
-    rectfill(screen,400,100,450,350,6);
-    rectfill(screen,450,300,550,350,6);
-
-    //Draw the castle
-    rectfill(screen,550,290,625,365,0);
-    circlefill(screen,545,285,18,0);
-    circlefill(screen,545,370,18,0);
-    circlefill(screen,630,285,18,0);
-    circlefill(screen,630,370,18,0);
-
-    //Draw the rectangle that will contain the citizens
-    rectfill(screen,560,300,615,350,9);
-
-    //Draw the tower build section of the screen
-    rectfill(screen,0,400,200,435,9);
-    textprintf_ex(screen,font,5,415,15,9,"Click this area");
-    textprintf_ex(screen,font,5,425,15,9,"to build a tower");*/
+    al_draw_line(640,480, 0, 0, al_map_rgb(255, 255, 255), 1000);
 
     al_register_event_source(fila_eventos, al_get_display_event_source(screen));
 
@@ -129,65 +120,56 @@ int main(void)
 
     al_flip_display();
 
-    while (1)
+    while (!GameOver)
     {
         ALLEGRO_EVENT evento;
         ALLEGRO_TIMEOUT timeout;
         al_init_timeout(&timeout, 0.05);
 
         int tem_eventos = al_wait_for_event_until(fila_eventos, &evento, &timeout);
-
         if (tem_eventos && evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
         {
             break;
         }
 
-        while(!GameOver)
-    {
-    //This is the main game loop
+            //This is the main game loop
 
-         //Create the Monstro
-         for(int x = 1; x < 11; x++)
-         {
-                 createmonster(x);
-         }
+            //Create the Monstro
+            for(int x = 1; x < 11; x++)
+            {
+                createmonster(x);
+            }
 
-         //Internal game loop to handle monster movement, shooting, etc.
-         for(int walkcount = 0; walkcount < 1500; walkcount ++)
-         {
+            //Internal game loop to handle monster movement, shooting, etc.
+            for(int walkcount = 0; walkcount < 1500; walkcount ++)
+            {
 //Did the player click on 'Build a tower'?
 
+                //Move the Monstro
+                for (int x = 0; x < 11; x++)
+                {
 
-                 //Move the Monstro
-                 for (int x = 0; x < 11; x++)
-                 {
+                    if (Monstro[x].stillalive == true)
+                    {
+                        erasemonster(x);
+                        movemonster(x);
+                        drawmonster(x);
+                        al_flip_display();
+                        printf("\nMonstro %d em execução\n", x);
+                        if (Monstro[x].xlocation > 529)
+                        {
+                            erasemonster(x);
+                            Monstro[x].stillalive = false;
+                            Monstro[x].xlocation = 0;
+                            Monstro[x].ylocation = 0;
 
-                     if (Monstro[x].stillalive)
-                     {
-                          erasemonster(x);
-                          movemonster(x);
-                          drawmonster(x);
-                          if (Monstro[x].xlocation > 529)
-                          {
-                               erasemonster(x);
-                               Monstro[x].stillalive = false;
-                               Monstro[x].xlocation = 0;
-                               Monstro[x].ylocation = 0;
-                               CitizenCounter--;
-                               UpdateCitizens();
-                               if (CitizenCounter == 0)
-                               {
-                                    while(!key[KEY_ESC])
-                                    {
-                                         textprintf_ex(screen,font,310,240,15,10,"GAME OVER!! Press ESCape to Exit");
-                                    }
-//                                    GameOver = true;
-                               }
-                          }
-                      }
-                  }
-         }
-     }
+
+
+                        }
+                    }
+                }
+            }
+
 
         al_flip_display();
     }
